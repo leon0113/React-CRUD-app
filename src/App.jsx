91e5bb637f3c674baf6a219981e4b5d6b623266c
Leon <tahjib.leon399@gmail.com>
2023-11-03 23:21:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold bg-orange-400 p-3 rounded-full'>React CRUD App</h1>
+    </>
+  )
+}
+
+export default App
